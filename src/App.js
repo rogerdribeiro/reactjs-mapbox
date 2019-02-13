801bb9689246abe5a06ef7282ad4383af0f9883a
Rogerd Ribeiro <rogerd.ribeiro@aluno.ufop.edu.br>
2019-02-13 09:14:01 -0200

    editorConfig, eslint, prettier

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <h1>Hello world</h1>;
+    return <h1>Hello</h1>;
   }
 }
 
